d43af9793fd445e1c63f2f807cffff8806d5c5f9
Adrian M2 <amac2008@gmail.com>
2024-10-29 15:21:51 -0700

    for loop correction

--- for_loops_example.py
+++ for_loops_example.py
@@ -1,4 +1,10 @@
 # Add all the numbers from 1-100 using a for loop
+# Expected output for (0,1,2,3,4,5)
+# 0+1=1
+# 1+2=3
+# 3+3=6
+# 6+4=10
+# 10+5=15  
 
 runningTotal = 0
 lowerBound = 0
@@ -9,11 +15,3 @@ for i in range(lowerBound, upperBound + 1):
     runningTotal = runningTotal + i
     
 print(f"The sum of all numbers from {lowerBound} to {upperBound} is {runningTotal}!")
-
-# 0,1,2,3,4,5
-# 0+1=1
-# 1+2=3
-# 3+3=6
-# 6+4=10
-# 10+5=15   
-

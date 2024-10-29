070f3d17c82e7aa7246243decdd787a0c136445a
Adrian M2 <amac2008@gmail.com>
2024-10-29 15:18:11 -0700

    for loop example

---
+++ for_loops_example.py
@@ -0,0 +1,19 @@
+# Add all the numbers from 1-100 using a for loop
+
+runningTotal = 0
+lowerBound = 0
+upperBound = 100
+
+for i in range(lowerBound, upperBound + 1):
+    # print(f"{runningTotal} + {i} = {runningTotal + i}") #OPTIONAL: Print each iteration
+    runningTotal = runningTotal + i
+    
+print(f"The sum of all numbers from {lowerBound} to {upperBound} is {runningTotal}!")
+
+# 0,1,2,3,4,5
+# 0+1=1
+# 1+2=3
+# 3+3=6
+# 6+4=10
+# 10+5=15   
+
